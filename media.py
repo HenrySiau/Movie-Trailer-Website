218cf21cd529e5da9a2f602791a0999f315780b8
HenrySiau <siauhenry@gmail.com>
2017-08-10 19:54:10 -0700

    formate media.py

--- media.py
+++ media.py
@@ -2,13 +2,17 @@ import webbrowser
 
 
 class Movie():
-    """ This class provides a way to store movie related information """
+    """This class provides a way to store movie related information."""
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(
+            self, movie_title, movie_storyline,
+            poster_image, trailer_youtube):
+        """Init."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Show."""
         webbrowser.open(self.trailer_youtube_url)
